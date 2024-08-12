1853f31c5e1f8c0b0125422897693a612cd1c592
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-08-12 14:23:34 +0530

    update the image and viewmodel for PolarChart in MAUI demo sample

--- Polar_chart_sample/PolarChartSample/ViewModel.cs
+++ Polar_chart_sample/PolarChartSample/ViewModel.cs
@@ -15,13 +15,13 @@ namespace PolarChartSample
             PlantDetails = new ObservableCollection<Model>()
             {
                 new Model(){ Direction = "North", Tree = 80, Flower = 42, Weed = 63},
-                new Model(){ Direction = "NorthWest", Tree = 85, Flower = 40, Weed = 70},
-                new Model(){ Direction = "West", Tree = 78 , Flower = 47, Weed = 65},
-                new Model(){ Direction = "SouthWest", Tree = 90 , Flower = 40, Weed = 70},
+                new Model(){ Direction = "NorthEast", Tree = 85, Flower = 40, Weed = 70},
+                new Model(){ Direction = "East", Tree = 78 , Flower = 47, Weed = 65},
+                new Model(){ Direction = "SouthEast", Tree = 90 , Flower = 40, Weed = 70},
                 new Model(){ Direction = "South", Tree = 78 , Flower = 27, Weed = 47},
-                new Model(){ Direction = "SouthEast", Tree = 83 , Flower = 45, Weed = 65},
-                new Model(){ Direction = "East", Tree = 79 , Flower = 40, Weed = 58},
-                new Model(){ Direction = "NorthEast", Tree = 88 , Flower = 38, Weed = 73}
+                new Model(){ Direction = "SouthWest", Tree = 83 , Flower = 45, Weed = 65},
+                new Model(){ Direction = "West", Tree = 79 , Flower = 40, Weed = 58},
+                new Model(){ Direction = "NorthWest", Tree = 88 , Flower = 38, Weed = 73}
             };
         }
     }
