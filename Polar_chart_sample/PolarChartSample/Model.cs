226cbd335835b2cf4805380cddfb5402a391670d
Kamalesh-Periyasamy <Kamalesh.periyasamy@syncfusion.com>
2026-03-26 14:33:41 +0530

    added new sample

---
+++ Polar_chart_sample/PolarChartSample/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarChartSample
+{
+    public class PlantModel
+    {
+        public string? Direction { get; set; }
+        public double Tree { get; set; }
+        public double Flower { get; set; }
+        public double Weed { get; set; }
+    }
+}
